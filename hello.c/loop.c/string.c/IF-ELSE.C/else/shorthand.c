3b4bc0edb06cab3687216338887cba313fad68ea
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 11:25:23 +0600

    while

--- hello.c/loop.c/string.c/IF-ELSE.C/else/shorthand.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int time = 20;
-    (time < 18) ? printf("good day") : printf("good evening");
-    return 0;
-}
--- hello.c/loop.c/string.c/copyString.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char str1[20] = "Hello World!";
-    char str2[20];
-    strcpy(str2, str1);
-    printf("%s", str2);
-    return 0;
-}
